132d290b159ce54600180fbe2c1f0cb62dac0687
razerblaze <ftapamahendra@gmail.com>
2022-11-25 22:07:16 +0800

    add field photo in user

--- models/user.go
+++ models/user.go
@@ -1,15 +1,19 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Username string `json:"username" form:"username"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-	IsAdmin  bool   `json:"isAdmin" form:"isAdmin"`
+	Username string    `json:"username" form:"username"`
+	Email    string    `json:"email" form:"email"`
+	Password string    `json:"password" form:"password"`
+	Photo    string    `json:"photo" form:"photo"`
+	IsAdmin  bool      `json:"isAdmin" form:"isAdmin"`
+	BanUntil time.Time `json:"banUntil" form:"banUntil"`
 }
 
 func (User) TableName() string {
